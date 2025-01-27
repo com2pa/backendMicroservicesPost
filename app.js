4ebc4bc3a3852d5138b30aa738c1804c3df36486
merwil vegas <com2pa@gmail.com>
2025-01-27 09:43:21 -0400

    endpoint

---
+++ app.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const app = express();
+const mongoose = require('mongoose');
+const usersRouter = require('./controllers/users');
+const cors = require('cors')
+const cookieParser = require('cookie-parser');
+const { usertExtractor } = require('./middleware/auth');
+const { MONGO_URL } = require('./config');
+const loginRouter = require('./controllers/login');
+const refresRouter = require('./controllers/refres');
+const logoutRouter = require('./controllers/logout');
+const postRouter = require('./controllers/posts');
+
+
+
+// const morgan=require('morgan')
+// conexion base de datos
+(async () => {
+  try {
+    await mongoose.connect(MONGO_URL);
+    console.log('Conectado a MongoDB :)');
+  } catch (error) {
+    console.log(error);
+  }
+})();
+
+app.use(cors())
+app.use(express.json());
+app.use(cookieParser())
+// app.use(morgan('tiny'))
+
+// rutas backEnd
+app.use('/api/users', usersRouter);
+app.use('/api/logout', logoutRouter);
+app.use('/api/login', loginRouter);
+app.use('/api/refres', usertExtractor, refresRouter)
+app.use('/api/post' ,usertExtractor, postRouter)
+
+
+app.use(express.static(path.resolve(__dirname, 'dist')));
+
+app.get('/*', function(request,response){
+  response.sendFile(path.resolve(__dirname, 'dist', 'index.html' ));
+});
+
+module.exports = app;
