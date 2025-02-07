3e3cfdfa48c00d8917515424219a7abdc9c96da3
merwil vegas <com2pa@gmail.com>
2025-02-07 16:48:49 -0400

    se cambio la url del domio de post

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const PAGE_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://microservicespost.onrender.com'
+    ? 'https://micro-post.onrender.com'
     : 'http://localhost:5173';
 
     const MONGO_URL = process.env.NODE_ENV === 'production'
