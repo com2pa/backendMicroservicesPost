aadd1fcc40238ac05bf8836829b4a131477f58bf
merwil vegas <com2pa@gmail.com>
2025-02-19 09:25:57 -0400

    endpoint de los microservicios

--- app.js
+++ app.js
@@ -24,7 +24,10 @@ const postRouter = require('./controllers/posts');
 })();
 
 const allowedOrigins = [
-  'https://blog-microservices.onrender.com',
+  'https://blog-microservices.onrender.com', // Frontend
+  'https://micro-post.onrender.com', // Otros microservicios
+  'https://micro-user-bju8.onrender.com',
+  'https://micro-comment.onrender.com',
   'http://localhost:5173',
 ];
 
