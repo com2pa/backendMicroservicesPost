7b607416d3ba537d7416b168c3750ea0e10655b6
merwil vegas <com2pa@gmail.com>
2025-02-22 11:06:28 -0400

    se modifico

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,32 +1,32 @@
-const jwt = require('jsonwebtoken')
-const User = require('../models/user')
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
-
-
-const usertExtractor = async(request, response, next)=>{
-    
-    try {
-        // comprobar que el token existe
-    const token = request.cookies?.accesstoken
-    if(!token) return response.status(401).json({error: 'No estas autorizado para acceder a esta ruta'})
- 
+const usertExtractor = async (request, response, next) => {
+  try {
+    // comprobar que el token existe
+    const token = request.cookies?.accesstoken;
+    console.log('obteniendo el token', token);
+    console.log('rec', request.cookies);
+    if (!token)
+      return response
+        .status(401)
+        .json({ error: 'No estas autorizado para acceder a esta ruta' });
 
     // verificar que el token es válido
-    
     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    // console.log(decoded)
+    console.log('verificando el token', decoded);
 
     // traer el usuario
     const user = await User.findById(decoded.id);
     request.user = user;
 
-
     // continuar con la ejecución de la ruta
     next();
-    } catch (error) {
-       return response.status(403).json({error: 'No estas autorizado para acceder a esta ruta'})
-    }
-    
-}
+  } catch (error) {
+    return response
+      .status(403)
+      .json({ error: 'No estas autorizado para acceder a esta ruta' });
+  }
+};
 
-module.exports = {usertExtractor};
+module.exports = { usertExtractor };
